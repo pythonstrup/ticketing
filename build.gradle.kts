f4b2cef3513879f9ef7901ba5abd0e7ebd509b61
pythonstrup <pythonstrup@gmail.com>
2024-05-11 23:10:02 +0900

    feat: spotless 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,52 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
     id("java")
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.5"
+    id("com.diffplug.spotless") version "6.25.0"
+    id("jacoco")
 }
 
-group = "com.ptu"
-version = "1.0-SNAPSHOT"
+val querydslDir = "**/src/main/generated/**"
+val bootJar: BootJar by tasks
+bootJar.enabled = false
 
-repositories {
-    mavenCentral()
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
-dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+allprojects {
+    group = "com.ptu"
+    version = "1.0"
+
+    repositories {
+        mavenCentral()
+    }
+}
+
+subprojects{
+    apply(plugin = "java")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "com.diffplug.spotless")
+
+    spotless {
+        java {
+            targetExclude(querydslDir)
+            importOrder()
+            removeUnusedImports()
+            googleJavaFormat()
+            trimTrailingWhitespace()
+            endWithNewline()
+        }
+    }
+
+    dependencies {
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    }
 }
 
 tasks.getByName<Test>("test") {
