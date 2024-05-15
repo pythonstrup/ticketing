c21b558f2b850e1f7baafa50b0427691fab11217
pythonstrup <pythonstrup@gmail.com>
2024-05-15 22:53:20 +0900

    feat: SecurityConfig - logout & exceptionHandling

--- api-shop/src/main/java/com/ptu/config/SecurityConfig.java
+++ api-shop/src/main/java/com/ptu/config/SecurityConfig.java
@@ -7,16 +7,43 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+  private final LogoutHandler logoutHandler;
+  private final LogoutSuccessHandler logoutSuccessHandler;
+
+  private final AccessDeniedHandler accessDeniedHandler;
+  private final AuthenticationEntryPoint authenticationEntryPoint;
+
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-    http.formLogin(AbstractHttpConfigurer::disable).httpBasic(AbstractHttpConfigurer::disable);
+    http.formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+    http.logout(
+        logout ->
+            logout
+                .logoutUrl("/api/v1/auth/logout")
+                .logoutSuccessHandler(logoutSuccessHandler)
+                .addLogoutHandler(logoutHandler));
+
+    http.exceptionHandling(
+        exceptionHandling ->
+            exceptionHandling
+                .authenticationEntryPoint(authenticationEntryPoint)
+                .accessDeniedHandler(accessDeniedHandler));
 
     return http.build();
   }
