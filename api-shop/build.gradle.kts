b14fccf01f4a7040ffd4422d385bc43ee7ec602d
pythonstrup <pythonstrup@gmail.com>
2024-05-15 22:31:49 +0900

    feat: 의존성 추가 및 수정

--- api-shop/build.gradle.kts
+++ api-shop/build.gradle.kts
@@ -21,14 +21,14 @@ dependencies {
     /**
      * JWTs
      */
-    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.5")
-    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 
     /**
      * JPA & Database
      */
-    runtimeOnly("mysql:mysql-connector-java")
+    runtimeOnly("com.mysql:mysql-connector-j")
     runtimeOnly("com.h2database:h2")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
