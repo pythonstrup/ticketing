35b34904cf9fbe02c2b4536207cfe04d432e7eca
pythonstrup <pythonstrup@gmail.com>
2024-05-15 23:15:49 +0900

    fix: 로깅 라이브러리 충돌해결

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,10 @@ subprojects{
         }
     }
 
+    configurations.configureEach {
+        exclude(group = "org.springframework.boot", module = "spring-boot-starter-logging")
+    }
+
     dependencies {
         testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
         testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
